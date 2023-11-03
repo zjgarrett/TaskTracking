0c360e2aaba78782530bd52a05e7d8308491fe2e
Zack Garrett <garre160@msu.edu>
2023-11-03 15:25:57 -0400

    Minor visual improvement

--- app/src/main/java/com/example/tasktracking/ui/attempt/TaskAttemptDetailScreen.kt
+++ app/src/main/java/com/example/tasktracking/ui/attempt/TaskAttemptDetailScreen.kt
@@ -1,14 +1,21 @@
 package com.example.tasktracking.ui.attempt
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,10 +24,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tasktracking.R
 import com.example.tasktracking.TaskTrackingTopAppBar
@@ -77,7 +87,7 @@ fun TaskAttemptDetailScreen(
         TaskAttemptDetailBody(
             taskAttemptUiState = taskAttemptUiState,
             modifier = Modifier
-                .padding(innerPadding)
+                .padding(top= innerPadding.calculateTopPadding(), bottom = 0.dp)
                 .fillMaxSize()
         )
     }
@@ -88,15 +98,23 @@ fun TaskAttemptDetailBody(
     taskAttemptUiState: TaskAttemptDetailUiState,
     modifier: Modifier = Modifier
 ) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
         IndividualTask(
-            taskAttemptUiState.task,
-            modifier
+            task = taskAttemptUiState.task,
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_small))
+                .wrapContentHeight()
         )
-        LazyColumn(modifier = modifier) {
-            items(items = taskAttemptUiState.attempted, key = {it}) {
+        Divider(thickness = Dp.Hairline)
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(items = taskAttemptUiState.attempted, key = { it.attemptId }) {
                 attempt ->
-                IndividualAttemptDetail(attempt = attempt, modifier = modifier)
+                IndividualAttemptDetail(
+                    attempt = attempt,
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)))
             }
         }
     }
@@ -107,9 +125,35 @@ fun IndividualAttemptDetail(
     attempt: Attempt,
     modifier: Modifier
 ) {
-    Row(modifier = modifier) {
-        Text(text = attempt.attemptDateStart.readableString())
-        Text(text = attempt.attemptDateEnd.readableString())
-        Text(text = attempt.completed.toString())
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Completed " + attempt.completed.toString() + " times",
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(R.string.starts, attempt.attemptDateStart.readableString()),
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text = stringResource(R.string.ends, attempt.attemptDateEnd.readableString()),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
     }
 }
